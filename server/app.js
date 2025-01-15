e5ae50867293561b1d9556ed733c7c23a2b310d5
Saidul147 <sidul.islam.146@gmail.com>
2025-01-15 14:50:12 +0600

    done

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(
       origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://emma-antigua-guys-fed.trycloudflare.com",
+        "https://shopus-sd.netlify.app",
       ],
     })
   );
